de391b3b1c0570b8f4b64a840057678bcad15cb7
Amy Wang <greenapple656@gmail.com>
2014-12-05 18:14:46 -0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -7,7 +7,7 @@ import javax.imageio.ImageIO;
 public class Test {
 
  public static void main(String[] args) throws IOException {
-  Image i = ImageIO.read(new File("stickfigure.png"));
+  Image i = ImageIO.read(new URL("https://cloud.githubusercontent.com/assets/8552282/5325501/5cc5a396-7caa-11e4-9f05-1662361a6899.png"));
   BufferedImage image = (BufferedImage) i;
   
   ShapedWindow sw = new ShapedWindow(image);
