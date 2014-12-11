e3fcc4889ee486faed8ea785a2a50ef77e57bf56
Amy Wang <greenapple656@gmail.com>
2014-12-11 12:56:55 -0700

    Create Test.java

---
+++ New/Test.java
@@ -0,0 +1,23 @@
+package shimeji;
+
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+
+import javax.imageio.ImageIO;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		Image i = ImageIO.read(new File("stickfigure.png"));
+		BufferedImage image = (BufferedImage) i;
+
+		//ShapedWindow sw = new ShapedWindow(image);
+		//sw.setVisible(true);
+
+		//Actions actions = new Actions(sw);
+		//actions.nextAction();
+		Mascot m = new Mascot(image);
+		m.nextAction();
+	}
+}
