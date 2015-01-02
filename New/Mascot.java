0546dc972404b9323a3fe2e12e8eeddf317b7d83
Amy Wang <greenapple656@gmail.com>
2015-01-02 16:09:49 -0700

    Update Mascot.java

--- New/Mascot.java
+++ New/Mascot.java
@@ -3,26 +3,25 @@ package shimeji;
 import javax.swing.*;
 
 import java.awt.*;
+import java.util.*;
 import java.awt.event.*;
+
+import javax.swing.event.*;
+
 import java.awt.image.*;
-import java.util.Random;
+
 
 public class Mascot extends JFrame {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private BufferedImage image = new BufferedImage(1, 1,
 			BufferedImage.TYPE_INT_ARGB);
-	private boolean flipped = false;
-	private ImageSets anims = new ImageSets();
+	private boolean stopped = false;
 
-	//constructors
-	public Mascot(){init();}
-	public Mascot(ImageSets i){anims=i;init();}
-	
-	//gets and sets
+	// constructors
+	public Mascot() {try {init();} catch (NoSuchMethodException e) {e.printStackTrace();}}
+
+	// gets and sets
 	public void setImage(BufferedImage bi) {
 		if (!bi.equals(null)) {
 			ImageIcon pic = new ImageIcon(bi);
@@ -31,27 +30,24 @@ public class Mascot extends JFrame {
 			this.setVisible(true);
 		}
 	}
-	public ImageSets getImageSet(){return anims;}
-	
-	//methods
-	public void init() {
-		image = anims.stand[0];
-		// window size = the image's size
+	public void setStopped(boolean b){stopped=b;}
+	/*temporary*/ public boolean getStopped(){return stopped;}
+
+	// methods
+	public void init() throws NoSuchMethodException {
+		Actions.init();
+		image = Actions.actionList[0].getImageSet()[0];
 		this.setSize(image.getWidth(), image.getHeight());
-		// currently, sets x to half of screen
-		// and y to right where it looks like it's standing on the bottom of the
-		// screen
 		this.setLocation(GraphicsEnvironment.getLocalGraphicsEnvironment()
 				.getMaximumWindowBounds().width / 2, GraphicsEnvironment
 				.getLocalGraphicsEnvironment().getMaximumWindowBounds().height
 				- image.getHeight());
-		// make image sets
-		anims.init();
-		// draws image on window
 		this.setContentPane(new JLabel(new ImageIcon(image)));
 
 		MouseInputListener listener = new MouseInputListener();
 		this.addMouseListener(listener);
+		this.addMouseMotionListener(listener);
+		
 		// gets rid of the borders, title bar, etc.
 		setUndecorated(true);
 		// gives it a transparent background, but doesn't transparentize the
@@ -63,110 +59,41 @@ public class Mascot extends JFrame {
 	}
 
 	public void nextAction() {
-		Random random = new Random();
-		int num = random.nextInt(3);
-		flipped = random.nextBoolean();
-		switch (num) {
-		case 0:
-			stand();
-			break;
-		case 1:
-			try {
-				walk();
-			} catch (InterruptedException e) {
-			}
-			break;
-		case 2:
-			try {
-				wave();
-			} catch (InterruptedException e) {
-			}
-			break;
-		default:
-			stand();
-			break;
+		while (!stopped) {
+			Random random = new Random();
+			int num = random.nextInt(3);
+			Actions.setCurrentMascot(this);
+			Actions.actionList[num].setContinue(1);
+			while(Actions.actionList[num].getContinue()!=0){Actions.actionList[num].go();}
+		}
+		while(stopped)
+		{
+			for(Action a : Actions.actionList){a.setContinue(0);}
 		}
 	}
-
-	//actions
-	public void stand() {
-
-		Random random = new Random();
-		// pauses for random time between 0.5 second and 3.5 seconds
-		int time = 100 * (random.nextInt(31) + 5);
-		try {
-			Thread.sleep(time);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+	
+	
+	class MouseInputListener extends MouseInputAdapter
+	{
+		int x;
+		int y;
+		public void mousePressed(MouseEvent e)
+		{
+			x=e.getX();
+			y=e.getY();
+			Mascot.this.setStopped(true);
+			System.out.println("Pressed");
 		}
-		nextAction();
-	}
-
-	public void walk() throws InterruptedException {
-		Random random = new Random();
-		while (flipped ? (this.getX() < (GraphicsEnvironment
-				.getLocalGraphicsEnvironment().getMaximumWindowBounds().width - this
-				.getWidth()))
-				: (this.getX() > 0)) {
-			for (BufferedImage b : flipped ? anims.walkR : anims.walk) {
-				this.setImage(b);
-				this.setLocation(
-						(flipped ? this.getX() + 10 : this.getX() - 10),
-						this.getY());
-				Thread.sleep(40);
-			}
-
-			int keep = random.nextInt(30);
-			if (keep == 0) {
-				nextAction();
-			}
+		public void mouseDragged(MouseEvent e)
+		{
+			Mascot.this.setLocation(e.getXOnScreen()-x,e.getYOnScreen()-y);
 		}
-		nextAction();
-	}
-
-	public void wave() throws InterruptedException {
-		Random random = new Random();
-		int keep = random.nextInt(8);
-		while (keep != 0) {
-			for (BufferedImage b : flipped ? anims.waveR : anims.wave) {
-				this.setImage(b);
-				Thread.sleep(300);
-			}
-
-			keep = random.nextInt(8);
-		}
-		nextAction();
-
-	}
-
-	public void fall() {
-		while (this.getY() > (GraphicsEnvironment.getLocalGraphicsEnvironment()
-				.getMaximumWindowBounds().height - this.getHeight())) {
-			this.setImage(flipped ? anims.fallR[0] : anims.fall[0]);
-			this.setLocation(this.getLocation().x, this.getLocation().y - 10);
-			try {
-				Thread.sleep(5);
-			} catch (InterruptedException e) {
-			}
-		}
-		if (anims.fall.length > 1) {
-			fallComplete();
-		}
-		nextAction();
-	}
-
-	private void fallComplete() {
-		for (BufferedImage b : flipped ? anims.fallR : anims.fall) {
-			this.setImage(b);
-			try {
-				Thread.sleep(30);
-			} catch (InterruptedException e) {
-			}
-		}
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
+		public void mouseReleased(MouseEvent e)
+		{
+			System.out.println("Released");
+			Mascot.this.setStopped(false);
+			Actions.fall();
+			Mascot.this.nextAction();
 		}
 	}
-
 }
