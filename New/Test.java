53a265226cb188341455100d2e83e54ee5799824
Amy Wang <greenapple656@gmail.com>
2015-01-02 16:10:07 -0700

    Update Test.java

--- New/Test.java
+++ New/Test.java
@@ -1,23 +1,22 @@
 package shimeji;
 
-import java.awt.*;
-import java.awt.image.*;
 import java.io.*;
-
-import javax.imageio.ImageIO;
+//import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) throws IOException {
-		Image i = ImageIO.read(new File("stickfigure.png"));
-		BufferedImage image = (BufferedImage) i;
+		//Image i = ImageIO.read(new File("stickfigure.png"));
+		//BufferedImage image = (BufferedImage) i;
 
 		//ShapedWindow sw = new ShapedWindow(image);
 		//sw.setVisible(true);
 
 		//Actions actions = new Actions(sw);
 		//actions.nextAction();
-		Mascot m = new Mascot(image);
+		Mascot m = new Mascot();
 		m.nextAction();
+		//Scanner scanner = new Scanner(System.in);
+		//while(scanner.hasNextInt()&&!m.getStopped()){}
 	}
 }
